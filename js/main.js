434e67d368fc6fa3b23bc726f54e4720e7d1ff83
Daniel Estrella Couto <estrellacouto05@gmail.com>
2023-09-22 17:44:31 -0300

    Fim do Projeto

--- js/main.js
+++ js/main.js
@@ -23,8 +23,10 @@ form.addEventListener ("submit", (evento)=>{
         itemAtual.id  = existe.id  
 
         atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento=> elemento.id=== id)] =  itemAtual
     } else {  
-        itemAtual.id = itens.length 
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 :0
 
         criaElemento(itemAtual)
 
@@ -47,11 +49,34 @@ function criaElemento (item){
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click",function(){
+        deletaElemento(this.parentNode, id)
+
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento=> elemento.id== id), 1)
+
+    localStorage.setItem("itens" , JSON.stringify(itens))
 }
